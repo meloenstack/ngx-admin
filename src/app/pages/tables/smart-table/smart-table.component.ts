8911f14424ec3ecba05be53568b6dc00cbc12aa7
meloenstack <carmelo.atienza@enstack.com>
2024-07-17 13:55:53 +0800

    feat: disable actions, apply init table style

--- src/app/pages/tables/smart-table/smart-table.component.ts
+++ src/app/pages/tables/smart-table/smart-table.component.ts
@@ -10,21 +10,26 @@ import { SmartTableData } from "../../../@core/data/smart-table";
 })
 export class SmartTableComponent {
   settings = {
-    add: {
-      addButtonContent: '<i class="nb-plus"></i>',
-      createButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-    },
-    edit: {
-      editButtonContent: '<i class="nb-edit"></i>',
-      saveButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-    },
-    delete: {
-      deleteButtonContent: '<i class="nb-trash"></i>',
-      confirmDelete: true,
-    },
+    // add: {
+    //   addButtonContent: '<i class="nb-plus"></i>',
+    //   createButtonContent: '<i class="nb-checkmark"></i>',
+    //   cancelButtonContent: '<i class="nb-close"></i>',
+    // },
+    // edit: {
+    //   editButtonContent: '<i class="nb-edit"></i>',
+    //   saveButtonContent: '<i class="nb-checkmark"></i>',
+    //   cancelButtonContent: '<i class="nb-close"></i>',
+    // },
+    // delete: {
+    //   deleteButtonContent: '<i class="nb-trash"></i>',
+    //   confirmDelete: true,
+    // },
+    actions: false,
     columns: {
+      firstName: {
+        title: "First Name",
+        type: "string",
+      },
       user_id: {
         title: "User ID",
         type: "number",
